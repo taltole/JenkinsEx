91f7bf124639ec510679cd30d2e3558c3bb394b3
tal <taltole@gmail.com>
2020-06-14 16:43:32 +0300

    fix issues

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import sys
 
 def main(arg):
     for i in range(arg):
-        print(f'{i} {i**2}')
+        print(f"{i} {i**2}")
 
 
-if __name__ == "__main":
+if __name__ == "__main__":
     arg = int(sys.argv[1])
     main(arg)
